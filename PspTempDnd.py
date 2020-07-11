a0fd04644ebb57442cbfabb832701d7925f95c30
poulsp <poulsp@gmail.com>
2020-07-11 16:57:06 +0200

    My initial commit

--- PspTempDnd.py
+++ PspTempDnd.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+import threading
+import json
+from core.commons import constants
 from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import IntentHandler
@@ -9,6 +13,51 @@ class PspTempDnd(AliceSkill):
 	Description: My own temporary dnd psptempdnd.
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
-		pass
+  _PUBLISH_WAIT_TIME = 1
+
+  #-----------------------------------------------
+  def __init__(self):
+    super().__init__()
+    self._timer = None
+    self._siteList = list()
+
+
+  #-----------------------------------------------
+  def onStart(self):
+    super().onStart()
+    self._siteList = self.getConfig('siteList').split(',')
+
+
+  #-----------------------------------------------
+  @IntentHandler('startMyDnd')
+  def startMyDnd(self, session: DialogSession, **_kwargs):
+    self.ThreadManager.doLater(
+      interval=PspDnd._PUBLISH_WAIT_TIME,
+      func=self._publishDnd,
+      args=[True]
+    )
+    #self.endDialog(session.sessionId, self.randomTalk('startMyDnd'))
+    self.endDialog(session.sessionId, '')
+
+
+  #-----------------------------------------------
+  @IntentHandler('stopMyDnd')
+  def stopMyDnd(self, session: DialogSession, **_kwargs):
+    self.ThreadManager.doLater(
+      interval=PspDnd._PUBLISH_WAIT_TIME,
+      func=self._publishDnd,
+      args=[False]
+    )
+    #self.endDialog(session.sessionId, self.randomTalk('stopMyDnd'))
+    self.endDialog(session.sessionId, '')
+
+
+  #-----------------------------------------------
+  def _publishDnd(self, onOff: bool):
+    if onOff:
+      for siteId in self._siteList:
+        self.publish(constants.TOPIC_HOTWORD_TOGGLE_OFF, payload=json.dumps({"siteId": siteId, "sessionId": ''}))
+    else:
+      for siteId in self._siteList:
+        self.publish(constants.TOPIC_HOTWORD_TOGGLE_ON, payload=json.dumps({"siteId": siteId, "sessionId": ''}))
+
