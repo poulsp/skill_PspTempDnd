b66a98703f482973d3d2f24d467490e48240909c
poulsp <poulsp@gmail.com>
2020-07-11 17:43:48 +0200

    Update and fixes

--- PspTempDnd.py
+++ PspTempDnd.py
@@ -13,51 +13,51 @@ class PspTempDnd(AliceSkill):
 	Description: My own temporary dnd psptempdnd.
 	"""
 
-  _PUBLISH_WAIT_TIME = 1
+	_PUBLISH_WAIT_TIME = 1
 
-  #-----------------------------------------------
-  def __init__(self):
-    super().__init__()
-    self._timer = None
-    self._siteList = list()
-
-
-  #-----------------------------------------------
-  def onStart(self):
-    super().onStart()
-    self._siteList = self.getConfig('siteList').split(',')
+	#-----------------------------------------------
+	def __init__(self):
+		super().__init__()
+		self._timer = None
+		self._siteList = list()
 
 
-  #-----------------------------------------------
-  @IntentHandler('startMyDnd')
-  def startMyDnd(self, session: DialogSession, **_kwargs):
-    self.ThreadManager.doLater(
-      interval=PspDnd._PUBLISH_WAIT_TIME,
-      func=self._publishDnd,
-      args=[True]
-    )
-    #self.endDialog(session.sessionId, self.randomTalk('startMyDnd'))
-    self.endDialog(session.sessionId, '')
+	#-----------------------------------------------
+	def onStart(self):
+		super().onStart()
+		self._siteList = self.getConfig('siteList').split(',')
 
 
-  #-----------------------------------------------
-  @IntentHandler('stopMyDnd')
-  def stopMyDnd(self, session: DialogSession, **_kwargs):
-    self.ThreadManager.doLater(
-      interval=PspDnd._PUBLISH_WAIT_TIME,
-      func=self._publishDnd,
-      args=[False]
-    )
-    #self.endDialog(session.sessionId, self.randomTalk('stopMyDnd'))
-    self.endDialog(session.sessionId, '')
+	#-----------------------------------------------
+	@IntentHandler('startMyDnd')
+	def startMyDnd(self, session: DialogSession, **_kwargs):
+		self.ThreadManager.doLater(
+			interval=PspTempDnd._PUBLISH_WAIT_TIME,
+			func=self._publishDnd,
+			args=[True]
+		)
+		#self.endDialog(session.sessionId, self.randomTalk('startMyDnd'))
+		self.endDialog(session.sessionId, '')
 
 
-  #-----------------------------------------------
-  def _publishDnd(self, onOff: bool):
-    if onOff:
-      for siteId in self._siteList:
-        self.publish(constants.TOPIC_HOTWORD_TOGGLE_OFF, payload=json.dumps({"siteId": siteId, "sessionId": ''}))
-    else:
-      for siteId in self._siteList:
-        self.publish(constants.TOPIC_HOTWORD_TOGGLE_ON, payload=json.dumps({"siteId": siteId, "sessionId": ''}))
+	#-----------------------------------------------
+	@IntentHandler('stopMyDnd')
+	def stopMyDnd(self, session: DialogSession, **_kwargs):
+		self.ThreadManager.doLater(
+			interval=PspTempDnd._PUBLISH_WAIT_TIME,
+			func=self._publishDnd,
+			args=[False]
+		)
+		#self.endDialog(session.sessionId, self.randomTalk('stopMyDnd'))
+		self.endDialog(session.sessionId, '')
+
+
+	#-----------------------------------------------
+	def _publishDnd(self, onOff: bool):
+		if onOff:
+			for siteId in self._siteList:
+				self.publish(constants.TOPIC_HOTWORD_TOGGLE_OFF, payload=json.dumps({"siteId": siteId, "sessionId": ''}))
+		else:
+			for siteId in self._siteList:
+				self.publish(constants.TOPIC_HOTWORD_TOGGLE_ON, payload=json.dumps({"siteId": siteId, "sessionId": ''}))
 
